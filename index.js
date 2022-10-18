669e771682c27d61b74e0a846be0429f9ff7bd89
Dilli_Rao <101472151+legendarycopyninja@users.noreply.github.com>
2022-10-19 00:41:52 +0530

    added new features to Functions

--- index.js
+++ index.js
@@ -1,50 +1,59 @@
-let card1 = getrandom();
-let card2 = getrandom();
 let sum = 0;
-let cards = [card1,card2];
+let cards = [];
 let hasBlackJack = false;
-let isAlive = true;
-let message = " ";
+let isAlive = false;
+let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
-sum = card1 + card2 ;
-
+//functio to get randomnumber for the cards
 function getrandom() {
  let randomdata = 0;
  randomdata = Math.floor(Math.random() * 13 ) + 1;
  if (randomdata === 1) {
-    return 11;
-}else if (randomdata > 10 ){
-    return 10;
+        return 11;
+    }else if (randomdata > 10 ){
+        return 10;
+    }else {
+        return randomdata;
 }
-  return randomdata;
 }
 
+//function to initilize cards and start game
 function startgame() {
- rendergame();
+    let card1 = getrandom();
+    let card2 = getrandom();
+    cards = [card1,card2];  
+    sum = card1 + card2 ;
+    isAlive = true;
+    rendergame();
 }
 
+//main function to rendergame and check card count for blakcjack
 function rendergame() {
     cardsEl.textContent = "CARDS : " ;
-    sumEl.textContent = "SUMS : " + sum;
-    
+     
     for (let i =0 ; i < cards.length;i++){
         cardsEl.textContent += cards[i] + " ";
     }
-    if(sum <= 20) {
+
+    sumEl.textContent = "SUMS : " + sum;
+
+    if (sum <= 20) {
         message = "Do you want to draw a new card? ";
-    } if (sum === 21 ) {
-        message = "You've got Blackjack!!!";
+    } else if (sum === 21 ) {
+        message = "Blackjack! Blackjack! Blackjack!";
         hasBlackJack = true ;     
     } else {
-        message = "No Blackjack ! oops";
+        message = "You're out of the game!";
         isAlive = false;
     }
+
     messageEl.textContent = message;
 }
 
+//function to draw a new card 
 function newgame(){
     let card3 = getrandom();
     sum += card3 ;
